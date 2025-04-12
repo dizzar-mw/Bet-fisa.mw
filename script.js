6bf08e94b32c9d2a6fd1a530a0c4dce3f1f6ca96
Desire Mapira <desiremapira@gmail.com>
2025-04-12 13:36:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const counter = document.getElementById('counter');
+let counNum = 0.01;
+const counterUpdate = setInterval(function(){
+  counNum++;
+  counter.textContent = "x"+counNum;
+},); 
